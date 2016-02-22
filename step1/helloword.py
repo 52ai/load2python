d8050b23e18f813cb068ee08f71777465394a856
2015E8007361074 <ieeflsyu@outlook.com>
2016-02-22 22:56:23 +0800

    元宵节第二次commit

--- step1/helloword.py
+++ step1/helloword.py
@@ -4,5 +4,15 @@
 	
 '''
 
-print 'Hello World'
-	
+print ('Hello World')
+
+print (100+200+300)
+
+print ('The quick brown fox','jumps over','the lazy dog')
+
+print ('100+200=',100+200)
+
+#name = input('please enter your name:')
+#print ('hello',name)
+
+print('1024 * 768 =',1024 * 768)
