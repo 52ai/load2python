fdbf60c7a6e78a75ea9afe39beddb887a53d61b0
2015E8007361074 <ieeflsyu@outlook.com>
2016-02-28 23:06:54 +0800

    add 001

---
+++ step1/001.py
@@ -0,0 +1,13 @@
+'''
+Created on 2016.2.28
+
+@author: Wenyan Yu
+'''
+# print absolute value of an Integer
+
+a = input('please input a number')
+a = int(a)
+if a >= 0:
+    print(a)
+else:
+    print(-a)
