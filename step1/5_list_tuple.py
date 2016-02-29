55df01d5206e74f5d828826017e150f8bd0d210f
2015E8007361074 <ieeflsyu@outlook.com>
2016-03-01 00:30:06 +0800

    20160301 first commit

---
+++ step1/5_list_tuple.py
@@ -0,0 +1,13 @@
+'''
+Created on 2016年2月29日
+
+@author: Wenyan Yu
+'''
+
+#list 是 Python 内置的数据类型
+
+classmates = ['Michael','Bob','Tracy']
+print(classmates)
+
+#变量classmates就是一个list。用len()函数可以获得list元素的个数
+print(len(classmates))
