73e920f61a16fc90a0def9591ebf9a2c7b4e6820
2015E8007361074 <ieeflsyu@outlook.com>
2016-02-29 16:34:17 +0800

    20160229 second commit

--- step1/variable.py
+++ step1/variable.py
@@ -22,3 +22,25 @@ print(b)
 
 PI = 3.14159265359
 print(PI)
+
+print(10 / 3)
+print(9 / 3)
+print(10 // 3) #//除法只取结果的整数部分
+print(10 % 3) #取余运算，很多语言里都有，就不要多说了
+
+
+
+#练习
+
+print('n = 123')
+print('f = 456.789')
+print("s1 = \'Hello,world\'")
+print("s2 = \'Hello,\\\'Adam\\\'\'")
+print("s3 = r\'Hello,\"Bart\"\'")
+print('''s4 = r\'\'\'Hello,
+Lisa!\'\'\'''')
+
+'''
+    Python的整数没有大小限制，Python的浮点数也没有大小限制，但是超出一定范围就直接表示为inf(无限大)
+'''
+
