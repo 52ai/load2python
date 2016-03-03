d4ac1a58aca520ce2c6e1dd8bc09c63560c120ed
2015E8007361074 <ieeflsyu@outlook.com>
2016-03-03 20:31:20 +0800

    20160303 第一次 提交

--- step1/8_dict_set.py
+++ step1/8_dict_set.py
@@ -21,3 +21,19 @@ print(d.get('Thomas','不存在该key'))
 #删除一个key
 d.pop('Bob')
 print(d)
+
+
+#set 是一组key的集合，但是不存储value.由于key不能重复，所以，在set中，没有重复的key
+s = set([1,2,3])
+print(s)
+
+s.add(4)
+print(s)
+
+s.remove(4)
+print(s)
+
+s1 = set([1,2,3])
+s2 = set([2,3,4])
+print(s1 & s2)
+print(s1 | s2)
