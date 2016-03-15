5f64b3ffd72775bf1a92d7e4381acc148ae10a1d
2015E8007361074 <ieeflsyu@outlook.com>
2016-03-15 18:34:20 +0800

    20160315 第一次提交

--- step1/b_23_decorator.py
+++ step1/b_23_decorator.py
@@ -7,6 +7,16 @@ Created on 2016年3月9日
 
 #因为一个《大秦帝国》，让我荒废了三天的时间,此时需要好好的反省
 
+import functools
+
+def log(func):
+    @functools.wraps(func)   #需要把原始函数的__name__等属性复制到wrapper()函数中，否则，有些依赖函数签名的代码执行就会出错
+    def wrapper(*args,**kw):
+        print('call %s:' % func.__name__)
+        return func(*args,**kw)
+    return wrapper
+
+@log    #now = log(now)
 def now():
     print('2016-3-13')
 
@@ -19,4 +29,12 @@ print(now.__name__)
 print(f.__name__)
 
 #今天跑步，欠下一天~~~~~~2016.3.14
+
+#在代码运行期间动态增加功能的方式，称之为装饰器（Decorator）
+
+now()
+print(now.__name__)
+
+
+
 
