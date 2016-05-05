e623cd40ccf4ecfe78fb257d6b16e62ce5540732
leefls yu <ieeflsyu@outlook.com>
2016-05-05 16:43:48 +0800

    learn python the hard way

--- Learn-python-the-hard-way/ex18.py
+++ Learn-python-the-hard-way/ex18.py
@@ -13,7 +13,7 @@ def print_two(*args):
 def print_two_again(arg1, arg2):
     print "arg1: %r, arg2: %r" % (arg1, arg2)
 
-
+    
 # this just takes one argument
 def print_one(arg1):
     print "arg1: %r" % arg1
