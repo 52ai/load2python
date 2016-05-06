781bf97a2d48999af72d8313ddcc627e7d9aafdb
leefls yu <ieeflsyu@outlook.com>
2016-05-07 02:56:33 +0800

    learn python the hard way

---
+++ Learn-python-the-hard-way/ex40.py
@@ -0,0 +1,29 @@
+# ex40 :Dictionaries, oh Lovely Dictionaries
+
+
+cities = {'CA': 'San Francisco', 'MI': 'Detroit', 'FL': 'Jacksonville'}
+
+cities['NY'] = 'New York'
+cities['OR'] = 'Portland'
+
+
+def find_city(themap, state):
+    if state in themap:
+        return themap[state]
+    else:
+        return "Not found"
+
+
+# OK pay attention!
+cities['_find'] = find_city
+
+while True:
+    print "State? (ENTER to quit)",
+    state = raw_input("> ")
+    if not state:break
+    # this line is the most important ever!study!
+    city_found = cities['_find'](cities, state)
+    print city_found
+
+
+print cities
