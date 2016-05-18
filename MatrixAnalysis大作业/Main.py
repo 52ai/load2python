ff09ee9c3e488993c2e42a2f3ded412e5548bedd
2015E8007361074 <ieeflsyu@outlook.com>
2016-05-18 21:41:46 +0800

    矩阵分析大作业

--- MatrixAnalysis大作业/Main.py
+++ MatrixAnalysis大作业/Main.py
@@ -7,7 +7,32 @@
 # 输出：所选择矩阵A对应分解或约减后的结果
 # 实例：A=[2 0 -1; -1 1 1; -1 2 2]
 #       A=[1 2 -3 4; 4 8 12 -8; 2 3 2 1; -3 -1 1 -4]
+#
+# 支持特性：1.支持n*n矩阵 2.支持浮点型运算
 # ###################################################
+from numpy import matrix
+
+
+def lu_factor(data):
+    print "LU"
+    print data
+
+
+def qr_factor(data):
+    print "QR"
+    print data
+
+
+def householder_reduction(data):
+    print "Householder"
+    print data
+
+
+def givens_reduction(data):
+    print "Givens"
+    print data
+
+
 
 print "温馨提示：1.请保证您正处于英文输入状态 2.请您参照下面给出的说明进行相应操作"
 print "- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -"
@@ -19,5 +44,18 @@ print "【4】Givens 约简"
 
 inputNum = raw_input("请选择：")
 print "- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -"
+# print inputNum
 
-print inputNum
+inputMatrix = raw_input("请输入一个矩阵：（同一行元素用空格隔开，不用行用分号隔开，例如：1 2 3; 3 4 5; 6 7 8）>")
+if not inputMatrix.strip():
+    inputMatrix = raw_input("输入矩阵不能为空,请重新输入> ")
+
+inputMatrix = inputMatrix.split(';')
+data = []
+for ele in inputMatrix:
+    data.append(map(float, ele.strip().split(' ')))
+
+if int(inputNum) == 1:
+    lu_factor(data)
+
+print data
