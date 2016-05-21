2f4b16b05e0049aef4cf5fc8d3c446326928be98
leefls yu <ieeflsyu@outlook.com>
2016-05-21 14:12:16 +0800

    矩阵分析大作业

--- MatrixAnalysis/Main.py
+++ MatrixAnalysis/Main.py
@@ -37,9 +37,10 @@ def lu_factor(data):
         for i in xrange(j, n):
             s2 = sum(U[k, j] * L[i, k] for k in xrange(j))
             L[i, j] = (A[i, j] - s2) / U[j, j]
-    print "--------------A---------------- \n", A
-    print "--------------L---------------- \n", L
-    print "--------------U---------------- \n", U, "\n -----------------------------"
+    print "分解公式：A = LU "
+    print "相应的A矩阵为：\n", A
+    print "相应的L矩阵为：\n", L
+    print "相应的U矩阵为：\n", U
     print "************************************"
     return (L, U)
 
@@ -66,9 +67,10 @@ def qr_factor(data):
             q = ak
             R[k-1, k-1] = sqrt(sum(x_i**2 for x_i in q)) #求q的模，并放在R矩阵的相应位置
             Q[:, k-1] = q / R[k-1, k-1] # 对矩阵Q的第k个列向量进行归一化操作
-    print "--------------A---------------- \n", A
-    print "--------------Q---------------- \n", Q
-    print "--------------R---------------- \n", R, "\n -----------------------------"
+    print "分解公式：A = QR "
+    print "相应的A矩阵为：\n", A
+    print "相应的Q矩阵为：\n", Q
+    print "相应的R矩阵为：\n", R
     print "************************************"
     return (Q, R)
 
@@ -112,35 +114,45 @@ def householder_reduction(data):
     P = R
     T = R * A
 
-    print "相应的P矩阵为：\n",P
-    print "相应的T矩阵为：\n",T
-    print "******************************************"
+    print "约简公式：PA = T "
+    print "相应的A矩阵为：\n", A
+    print "相应的P矩阵为：\n", P
+    print "相应的T矩阵为：\n", T
+    print "************************************"
     return (P, T)
 
 def givens_reduction(data):
     A = data
     print "**********Givens_reduction***********"
-    [m, n] = A.shape
-    
+    [m, n] = A.shape # 获得矩阵的大小
     #初始化Q矩阵
-    Q = matrix([[float( i == j) for i in xrange(n)] for j in xrange(n)])
-    a = A
-    xk = a[:,1]
-    for k in xrange(1,n-1):
-        Ak = a[k-1:m, k:n]
-        xk = Ak(:, 1)
-        [n1, n2] = size(Ak)
+    Q = matrix([[float( i == j) for i in xrange(m)] for j in xrange(m)])
+    a = A # 初始化a
+    xk = a[:,0] # 初始化xk为a 的第一个列向量
+    for k in xrange(1,n):
+        Ak = a[k-1:m, k-1:n] #选取相应的子矩阵
+        xk = Ak[:, 0] # 更新xk为Ak的第一个列向量
+        [n1, n2] = Ak.shape
         Qm = matrix([[float(i==j) for i in xrange(n1)] for j in xrange(n1)])
-        for m in xrange(2, n1):
+        for m in xrange(2, n1 + 1):
             q = matrix([[float(i==j) for i in xrange(n1)] for j in xrange(n1)])
-            q[0, 0] = xk[0] 
-
-            
-
-
-    print "相应的Q矩阵为：\n",Q
-    print "相应的T矩阵为：\n",T
-    print "******************************************"
+            q[0, 0] = xk[0]/sqrt(xk[0]**2 + xk[m-1]**2)
+            q[0, m-1] = xk[m-1] / sqrt(xk[0]**2 + xk[m-1]**2)
+            q[m-1, 0] = -xk[m-1] /sqrt(xk[0]**2 + xk[m-1]**2)
+            q[m-1, m-1] = xk[0]/ sqrt(xk[0]**2 + xk[m-1]**2)
+            Ak = q*Ak # 更新a 为R(ij) * a
+            xk = Ak[:, 0] #更新xk 为R(ij)*a之后的第一列
+            Qm= q * Qm
+        a = Ak
+        Qk = matrix([[float(i == j) for i in xrange(n)] for j in xrange(n)])
+        Qk[k-1:n,k-1:n] = Qm # 对n>=2时的R进行更新操作
+        Q = Qk * Q
+    T = Q * A
+    print "约简公式：QA = T "
+    print "相应的A矩阵为：\n", A
+    print "相应的Q矩阵为：\n", Q
+    print "相应的T矩阵为：\n", T
+    print "************************************"
     return (Q, T)
 
 print "温馨提示：1.请保证您正处于英文输入状态 2.请您参照下面给出的说明进行相应操作"
@@ -168,7 +180,8 @@ for ele in inputMatrix:
 # data = ('2 2 2; 4 7 7; 6 18 22') # for LU n*n
 # data = ('0 -20 -14; 3 27 -4')  # for QR(GramSchidt) n*n
 # data = ('1 1 0; 1 0 1') for QR(GramSchidt) m*n
-data = ('0 -20 -14; 3 27 -4; 4 11 -2') # for Householder and Givens
+# data = ('0 -20 -14; 3 27 -4; 4 11 -2') # for Householder and Givens
+data = ('1 19 -34; -2 -5 20; 2 8 37') # for LU QR Householder and Givens
 data = matrix(data)
 
 if inputNum == 1:
