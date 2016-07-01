12ae94210f85d2bcb6aacaf6ca7adb4aa83ddc14
2015E8007361074 <ieeflsyu@outlook.com>
2016-07-01 21:20:38 +0800

    ml&&python:20160701

---
+++ BeginningPython/Module/calculate.py
@@ -0,0 +1,5 @@
+# coding = utf-8
+from fibo import fib
+
+for n in range(10, 50, 5):
+    fib(n)
---
+++ BeginningPython/Module/fibo.py
@@ -0,0 +1,14 @@
+# coding = utf-8
+
+print "__name__:", __name__
+
+
+def fib(n):
+    a, b = 0, 1
+    result = []
+    print "in fib():", __name__
+    while b < n:
+        result.append(b)
+        a, b = b, a+b
+    print result
+
