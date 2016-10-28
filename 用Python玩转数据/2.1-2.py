f8a792cbf4100e95c920e1809daa2ff5055120c7
52ai <ieeflsyu@outlook.com>
2016-10-28 10:58:49 +0800

    用python 玩转数据

--- 用Python玩转数据/2.1-2.py
+++ 用Python玩转数据/2.1-2.py
@@ -1,6 +1,10 @@
 # coding:utf-8
 
 import urllib
-r = urllib.urlopen("http://mryu.top/")
-html = r.read()
-print html
+import os
+# r = urllib.urlopen("http://mryu.top/")
+# html = r.read()
+# print html
+
+print os.getcwd()
+print dir()
---
+++ 用Python玩转数据/2.1-exercise2.py
@@ -0,0 +1,10 @@
+import urllib2
+import re
+dStr = urllib2.urlopen('https://hk.finance.yahoo.com/q/cp?s=%5EDJI').read()
+m = re.findall('<tr><td class="yfnc_tabledata1"><b><a href=".*?">(.*?)</a></b></td><td class="yfnc_tabledata1">(.*?)</td>.*?<b>(.*?)</b>.*?</tr>', dStr)
+if m:
+    print m
+    print '\n'
+    print len(m)
+else:  
+	print "not match"
