08871b71cb8cd53b74bf2a766e976c41e52c3e23
52ai <ieeflsyu@outlook.com>
2016-12-12 14:34:14 +0800

    用Python玩转PyQt GUI编程

--- PythonGUI学习/PyQt4/HelloPyQt.py
+++ PythonGUI学习/PyQt4/HelloPyQt.py
@@ -13,9 +13,9 @@ from PyQt4 import QtGui
 def main():    
     app = QtGui.QApplication(sys.argv)
     w = QtGui.QWidget()
-    w.resize(250, 150)
-    w.move(300, 300)
-    w.setWindowTitle('Simple')
+    w.resize(400, 300)
+    w.move(800, 300)
+    w.setWindowTitle(u'这是一个标题！')
     w.show()
     
     sys.exit(app.exec_())
