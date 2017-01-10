567332b55f89b6a3526ef30a005673956c0135af
52ai <ieeflsyu@outlook.com>
2017-01-10 18:54:27 +0800

    用Scrapy 进行网络数据采集

--- 用Python写网络爬虫/scrapetest.py
+++ 用Python写网络爬虫/scrapetest.py
@@ -1,4 +1,5 @@
-from urllib.request import urlopen
-#Retrieve HTML string from the URL
+from urllib2 import urlopen
+
+# Retrieve HTML string from the URL
 html = urlopen("http://www.pythonscraping.com/exercises/exercise1.html")
 print(html.read())
