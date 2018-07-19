e465c8097cef670b1d53c34e98a19739214a0900
Wayne Yu <ieeflsyu@outlook.com>
2018-07-19 12:22:17 +0800

    using Pycharm again in Acer, cool!

--- BeginningPython/test.py
+++ BeginningPython/test.py
@@ -1,9 +1,10 @@
 # coding:utf-8
 import sys
-import django
+# import django
 print "Hello, Python!"
 print dir()
 print sys.version
 print sys.version_info
 print "Hello, Sublime text 3"
-print django.get_version()
+# print django.get_version()
+print "Hello Pycharm on Windows!"
