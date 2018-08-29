e970def42507214f388e059bc1e2fdaa3ee04cb9
Wayne Yu <ieeflsyu@outlook.com>
2018-08-29 18:14:04 +0800

    hello, urllib2

--- 用Python写网络爬虫/scrapetest.py
+++ 用Python写网络爬虫/scrapetest.py
@@ -3,4 +3,4 @@ from urllib2 import urlopen
 # Retrieve HTML string from the URL
 html = urlopen("http://www.pythonscraping.com/exercises/exercise1.html")
 print(html.read())
-
+print "hello, urllib2"
